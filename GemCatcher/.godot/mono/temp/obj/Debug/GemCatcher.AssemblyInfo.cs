545d8e57f9889d26e4d7c21002081a6e5b0c155b
kelpiekrisps <hoflaherty@ucdavis.edu>
2025-03-03 15:01:37 -0800

    started Tappy, imported assets

--- GemCatcher/.godot/mono/temp/obj/Debug/GemCatcher.AssemblyInfo.cs
+++ GemCatcher/.godot/mono/temp/obj/Debug/GemCatcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GemCatcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebdf9ce74926576695b25d84b5748b79569d1d3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GemCatcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GemCatcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
